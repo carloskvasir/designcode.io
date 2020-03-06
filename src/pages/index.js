dc178f3e433340dedc21f74a70739b66bd0a0021
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2020-03-06 09:56:05 -0300

    04 - Interactions and Animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,9 @@ const IndexPage = () => (
     <div className="Hero">
       <div className="HeroGroup">
         <SEO title="Home" />
-        <h1>Learn to design and code React apps</h1>
+        <h1>
+          Learn to <br /> design and code React apps
+        </h1>
         <p>
           Complete courses about the best tools and design systems. Prototype
           and build apps with React and Swift.
