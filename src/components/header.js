704b7d34ddb3b42d32aa2918f2fffe32ade29d17
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2020-03-05 23:59:34 -0300

    03 - Adaptive Layouts with CSS Grid

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,12 @@ import { Link, Img } from 'gatsby'
 import React from 'react'
 
 import './header.scss'
-import logo from '../images/logo-designcode.svg'
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
       <Link to="/">
-        <img src={logo} width="30" />
+        <img src={require('../images/logo-designcode.svg')} width="30" />
       </Link>
       <Link to="/courses">Courses</Link>
       <Link to="/downloads">Downloads</Link>
